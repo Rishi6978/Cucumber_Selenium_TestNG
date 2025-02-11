46690da09a6c1afe3ddaa586e31202369a0b1233
surya bhaskar <contact@surya.bchilukuri@gmail.com>
2025-02-10 20:11:47 -0700

    commit

--- src/main/java/com/example/pom/login.java
+++ src/main/java/com/example/pom/login.java
@@ -7,7 +7,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-//import org.testng.Assert;
+import org.testng.Assert;
+
 import java.security.PrivateKey;
 import java.util.Map;
 import java.util.Properties;
